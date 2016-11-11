7102c74039f02cfb15a93e4c553695ef85a80acd
holger krekel <holger@merlinux.eu>
2016-11-11 14:52:00 +0100

    bump version, amend changelog

--- pluggy.py
+++ pluggy.py
@@ -67,7 +67,7 @@ Pluggy currently consists of functionality for:
 import sys
 import inspect
 
-__version__ = '0.4.1'
+__version__ = '0.5.0.dev0'
 
 __all__ = ["PluginManager", "PluginValidationError", "HookCallError",
            "HookspecMarker", "HookimplMarker"]
