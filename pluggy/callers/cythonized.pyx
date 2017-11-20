844412aab6539bbf82c6c2354bdf935738a34ec9
Tyler Goodlet <tgoodlet@gmail.com>
2017-11-20 15:50:52 -0500

    Cythonize the call loop
    Cythonize `pluggy.callers._multicall` which gives around a 2x speedup
    according to the benchmark suite. Transform the `pluggy.callers`
    module in a new package and move utils and the legacy call loop into
    separate modules.

---
+++ pluggy/callers/cythonized.pyx
@@ -0,0 +1,60 @@
+"""
+Cynthonized hook call loop.
+
+NOTE: In order to build this source you must have cython installed.
+"""
+import sys
+from . import _Result, _raise_wrapfail, HookCallError
+
+
+cpdef _multicall(list hook_impls, dict caller_kwargs, bint firstresult=False):
+    """Execute a call into multiple python functions/methods and return the
+    result(s).
+
+    ``caller_kwargs`` comes from _HookCaller.__call__().
+    """
+    __tracebackhide__ = True
+    results = []
+    excinfo = None
+    try:  # run impl and wrapper setup functions in a loop
+        teardowns = []
+        try:
+            for hook_impl in reversed(hook_impls):
+                try:
+                    args = [caller_kwargs[argname] for argname in hook_impl.argnames]
+                except KeyError:
+                    for argname in hook_impl.argnames:
+                        if argname not in caller_kwargs:
+                            raise HookCallError(
+                                "hook call must provide argument %r" % (argname,))
+
+                if hook_impl.hookwrapper:
+                    try:
+                        gen = hook_impl.function(*args)
+                        next(gen)   # first yield
+                        teardowns.append(gen)
+                    except StopIteration:
+                        _raise_wrapfail(gen, "did not yield")
+                else:
+                    res = hook_impl.function(*args)
+                    if res is not None:
+                        results.append(res)
+                        if firstresult:  # halt further impl calls
+                            break
+        except BaseException:
+            excinfo = sys.exc_info()
+    finally:
+        if firstresult:  # first result hooks return a single value
+            outcome = _Result(results[0] if results else None, excinfo)
+        else:
+            outcome = _Result(results, excinfo)
+
+        # run all wrapper post-yield blocks
+        for gen in reversed(teardowns):
+            try:
+                gen.send(outcome)
+                _raise_wrapfail(gen, "has second yield")
+            except StopIteration:
+                pass
+
+        return outcome.get_result()
