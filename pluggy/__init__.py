8029ab004ddd7e863980419d1adeacb9b819d437
Bruno Oliveira <nicoddemus@gmail.com>
2018-04-21 17:45:04 -0300

    Use setuptools_scm to automatically version the package on tags

--- pluggy/__init__.py
+++ pluggy/__init__.py
@@ -1,4 +1,9 @@
-__version__ = '0.6.1.dev'
+try:
+    from ._version import version as __version__
+except ImportError:
+    # broken installation, we don't even try
+    # unknown only works because we do poor mans version compare
+    __version__ = 'unknown'
 
 __all__ = ["PluginManager", "PluginValidationError", "HookCallError",
            "HookspecMarker", "HookimplMarker"]
