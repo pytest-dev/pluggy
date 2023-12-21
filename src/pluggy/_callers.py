d3877d47a0cd2a142c9d63c97c46d73b97d1b423
Ran Benita <ran@unusedvar.com>
2023-12-21 12:31:52 +0200

    Move `_raise_wrapfail` to callers.py
    It's only used there.

--- src/pluggy/_callers.py
+++ src/pluggy/_callers.py
@@ -6,12 +6,12 @@ from __future__ import annotations
 from typing import cast
 from typing import Generator
 from typing import Mapping
+from typing import NoReturn
 from typing import Sequence
 from typing import Tuple
 from typing import Union
 
 from ._hooks import HookImpl
-from ._result import _raise_wrapfail
 from ._result import HookCallError
 from ._result import Result
 
@@ -22,6 +22,19 @@ Teardown = Union[
     Tuple[Generator[None, Result[object], None]],
     Generator[None, object, object],
 ]
+
+
+def _raise_wrapfail(
+    wrap_controller: (
+        Generator[None, Result[object], None] | Generator[None, object, object]
+    ),
+    msg: str,
+) -> NoReturn:
+    co = wrap_controller.gi_code
+    raise RuntimeError(
+        "wrap_controller at %r %s:%d %s"
+        % (co.co_name, co.co_filename, co.co_firstlineno, msg)
+    )
 
 
 def _multicall(
